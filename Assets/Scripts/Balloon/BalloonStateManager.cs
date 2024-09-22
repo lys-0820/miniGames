5bf5562c1ba84272c8e9aa1a4b53dcf687740ab5
AlinaQureshi <qureshi.alina3@gmail.com>
2024-09-22 12:20:11 +0100

    [UPDATE] Updated balloon game with visuals + upward movement

---
+++ Assets/Scripts/Balloon/BalloonStateManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OkapiKit;
+
+public class BalloonStateManager : MonoBehaviour
+{
+    [SerializeField] private Hypertag _obstacleTag;
+    [SerializeField] private GameTimer _gameTimer;
+
+    void Start()
+    {
+        _gameTimer.MarkGameAsSuccess();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log(collider.gameObject.name);
+        if (collider.gameObject.HasHypertag(_obstacleTag))
+        {
+            Debug.Log("collided");
+            _gameTimer.MarkGameAsFailure();
+        }
+    }
+}
